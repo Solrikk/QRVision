93c64b35dd9b87485b92ff8ea12cfd7d51c15b9b
Sviatoslav König <reveni324@gmail.com>
2024-05-21 11:07:10 +0300

    Add files via upload

---
+++ static/script.js
@@ -0,0 +1,38 @@
+const video = document.getElementById('video');
+const captureBtn = document.getElementById('captureBtn');
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+
+navigator.mediaDevices.getUserMedia({ video: { width: 640, height: 360 } })
+    .then(stream => {
+        video.srcObject = stream;
+    })
+    .catch(err => {
+        console.error("Ошибка доступа к камере: ", err);
+    });
+
+captureBtn.addEventListener('click', () => {
+    setTimeout(() => {
+        canvas.style.display = 'block';
+        ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
+
+        canvas.toBlob((blob) => {
+            const formData = new FormData();
+            formData.append('file', blob, 'capture.png');
+
+            fetch('/scan-qr/', {
+                method: 'POST',
+                body: formData
+            })
+            .then(response => response.json())
+            .then((data) => {
+                alert('Данные QR-кода: ' + data.data);
+                canvas.style.display = 'none';
+            })
+            .catch(error => {
+                console.error('Ошибка:', error);
+                canvas.style.display = 'none';
+            });
+        }, 'image/png');
+    }, 500);
+});

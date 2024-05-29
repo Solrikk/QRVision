a2ff08d993d1ea63c4c6e3dc59a5eb3cf61d3032
Sviatoslav König <reveni324@gmail.com>
2024-05-30 00:37:30 +0300

    # add: improved replit.nix

--- replit.nix
+++ replit.nix
@@ -1,17 +1,16 @@
 { pkgs }: {
   deps = [
+    pkgs.postgresql
+    pkgs.glibcLocales
+    pkgs.zbar
     pkgs.python38Full
   ];
   env = {
     PYTHON_LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [
-      # Neded for pandas / numpy
       pkgs.stdenv.cc.cc.lib
       pkgs.zlib
-      # Needed for pygame
       pkgs.glib
-      # Needed for matplotlib
       pkgs.xorg.libX11
-      # Needed for opencv-python
       pkgs.libGL
     ];
     PYTHONBIN = "${pkgs.python38Full}/bin/python3.8";
